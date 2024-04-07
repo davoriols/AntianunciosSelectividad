00873ed3a156c9b7a8bfa974866625013ea6da81
davoriols <d4c7ooaz@anonaddy.me>
2024-04-07 18:35:30 +0200

    Arreglado fallo que impedia ver las soluciones de ejercicios

--- antiexamens.js
+++ antiexamens.js
@@ -1,24 +1,38 @@
-// Poner fuente tamaño 0 para el baner en los examenes
+// Selecciona los diferentes elementos de la página web a través de las clases y le 
+// cambia la fuente o no lo renderiza:
 var style = document.createElement('style');
 style.innerHTML = `
+/* La watermark de "examenselectivitat.cat */
 text.font-rubik{
   font-size: 0;
 }
 
+/* Carroussell de arriba de Aprofita el temps */ 
 .v-window__container {
-  display: none;
+  display: none !important;
 }
 
 /* Carroussell con las libretas */
-.mt-6.problem-card.v-card.v-sheet.theme--light {
+.px-10.pt-8{
     display: none;
 }
 
-/* Anuncio de que no salgan más anuncios */
+.slide-suggested.py-5.prevent-select.mt-6.v-item-group.theme--light.v-slide-group.v-slide-group--has-affixes.v-slide-group--is-overflowing{
+    display: none;
+
+}
+
+/* Anuncio de que no salgan más anuncios */ 
 .v-image.v-responsive.rounded-lg.mt-5.theme--light {
     display: none;
 }
 
+/* Anuncio de libretas que sale cuando seleccionas exámenes */
+.align-center.rounded-lg.my-6.v-card.v-card--flat.v-card--link.v-sheet.theme--light.d-flex.pa-6 {
+  display: none;
+  position: absolute;
+  left: 100000px;
+}
 
 `;
 
